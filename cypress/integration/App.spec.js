e35edb8d107cd532e8112169f7f5d0d7924e5295
Colin Morrison <colinjmmorris@gmail.com>
2021-04-22 18:23:51 +0100

    Unit tests good. Integration testing started

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -7,4 +7,79 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('arithmetical operations should update the display with the result of the operation', () => {
+    // If we press 5, multiply, 8, and equals, the calculator .display should contain '40'
+    cy.get('#number5').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number8').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '40')
+  })
+
+  // Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
+  it('output is expected after multiple calculations un running total', () => {
+    cy.get('#number9').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number3').click();
+    cy.get('#operator_equals').click();
+    cy.get('#operator_add').click();
+    cy.get('#number7').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '13')
+  })
+
+  it('output can be negative', () => {
+    cy.get('#number5').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number9').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '-4')
+  })
+
+
+  it('output can be decimal', () => {
+    cy.get('#number9').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number5').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '1.8')
+
+  })
+
+  it('output can be large', () => {
+    cy.get('#number8').click();
+    cy.get('#number7').click();
+    cy.get('#number3').click();
+    cy.get('#number4').click();
+    cy.get('#number4').click();
+    cy.get('#number8').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number5').click();
+    cy.get('#number9').click();
+    cy.get('#number2').click();
+    cy.get('#number9').click();
+    cy.get('#number7').click();
+    cy.get('#number5').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '517932827800')
+  })
+
+  // What does the code do in exceptional circumstances? - Output for dividing by zero is 'Infinity'
+
+  // I want it to instead display 'undefined'
+  it('dividing by zero returns string "undefined"', () => {
+    cy.get('#number8').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number0').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', 'undefined')
+  })
 })
+
+
+// What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? 
+// Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass
+// (you will need to modify the Calculator model to meet this requirement).
+
+
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -14,5 +14,72 @@ describe('Calculator', () => {
     expect(runningTotal).toHaveTextContent('4');
     
   })
-})
 
+  it('should add 1 and 4 together to get 5', () => {
+    // Grab all the buttons I want to press (1, 4, add, equals)
+    // Grab the runningTotal again
+    // Fire each event in turn
+    // Check that the runningTotal matches up with what we expect (5)
+    const button1 = container.getByTestId('number1');
+    const button4 = container.getByTestId('number4');
+    const buttonAdd = container.getByTestId('add');
+    const buttonEquals = container.getByTestId('equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button1);
+    fireEvent.click(buttonAdd);
+    fireEvent.click(button4);
+    fireEvent.click(buttonEquals);
+    expect(runningTotal).toHaveTextContent('5');
+  })
+
+  it('should subtract 4 from 7 to get 3', () => {
+    const button7 = container.getByTestId('number7');
+    const button4 = container.getByTestId('number4');  
+    const buttonSubtract = container.getByTestId('subtract');
+    const buttonEquals = container.getByTestId('equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button7);
+    fireEvent.click(buttonSubtract);
+    fireEvent.click(button4);
+    fireEvent.click(buttonEquals);
+    expect(runningTotal).toHaveTextContent('3');
+  })
+
+  it('should multiple 3 by 5 to get 15', () => {
+    const button3 = container.getByTestId('number3');
+    const button5 = container.getByTestId('number5');
+    const buttonMultiply = container.getByTestId('multiply');
+    const buttonEquals = container.getByTestId('equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button3);
+    fireEvent.click(buttonMultiply);
+    fireEvent.click(button5);
+    fireEvent.click(buttonEquals);
+    expect(runningTotal).toHaveTextContent('15');
+  })
+
+  it('should divide 21 by 7 to get 3', () => {
+    // 21 requires buttons 2 and 1 to be fired before the divide, then button 7
+    const button2 = container.getByTestId('number2');
+    const button1 = container.getByTestId('number1');
+    const button7 = container.getByTestId('number7');
+    const buttonDivide = container.getByTestId('divide');
+    const buttonEquals = container.getByTestId('equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button2);
+    fireEvent.click(button1);
+    fireEvent.click(buttonDivide);
+    fireEvent.click(button7);
+    fireEvent.click(buttonEquals);
+    expect(runningTotal).toHaveTextContent('3');
+  })
+
+  it('should concatenate multiple number button clicks', () => {
+    // Grab button 4, fire it twice, compare running total
+    const button4 = container.getByTestId('number4');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button4);
+    fireEvent.click(button4);
+    expect(runningTotal).toHaveTextContent('44');
+  })
+})
